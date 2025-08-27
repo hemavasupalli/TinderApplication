c27c9134460dfb4e53cff7fd59adb72fea76487f
Hema vasupalli <hemavasupalli@Hemas-MacBook-Pro.local>
2025-08-27 12:47:54 -0400

    signup

--- src/app.js
+++ src/app.js
@@ -1,18 +1,47 @@
-const express = require('express');
-const connectDB = require("./config/database")
+const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
 // app.use("/test",(req,res)=>{
 //     res.send("Hi ram ")
 // })
 
-connectDB().then(()=>{
-    console.log("Database successfully connected")
-    app.listen(3000,()=>{
-        console.log("server started")
+app.post("/signup", async (req, res) => {
+  const userObj = {
+    firstName: 11,
+    lastName: "vasupalli",
+    emailId: "anil@gmail.com",
+    password: "anil@1234",
+    hjel:"7"
+  };
+  //creatung new instance of user model
+  const user = new User(userObj);
+  try {
+    await user.save();
+    res.send("user added");
+  } catch (err) {
+    res.status(400).send("error saving the user", err.message);
+  }
+});
+
+// app.get("/users",async(req,res)=>{
+//     try{
+//         await user.save();
+//     res.send("user added")
+//     }catch(err){
+//         res.status(400).send("error saving the user", err.message)
+
+//     }
+// })
+
+connectDB()
+  .then(() => {
+    console.log("Database successfully connected");
+    app.listen(3000, () => {
+      console.log("server started");
     });
-}).catch(err=>{
-    console.log("Database cannot be connected")
-
-})
-
+  })
+  .catch((err) => {
+    console.log("Database cannot be connected");
+  });
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async()=>{
-    await mongoose.connect("mongodb+srv://hemavasupalli12:Homosap%402024@clusterdevtinder.tpldqxf.mongodb.net/DevTinder")
+    await mongoose.connect("mongodb+srv://hemavasupalli12:Homosap%402024@clusterdevtinder.tpldqxf.mongodb.net/devTinder")
 };
 
 module.exports = connectDB;
