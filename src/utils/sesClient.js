39fe542bb80a5cc776b147b51c0ef5ae23218f9b
Hema vasupalli <hemavasupalli@Hemas-MacBook-Pro.local>
2025-09-06 17:49:14 -0400

    Added email teemplate

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -1,8 +1,9 @@
 
 const { SESClient } = require("@aws-sdk/client-ses");
+require("dotenv").config();
 
 // Create SES service object.
-const sesClient = new SESClient({ region: process.env.REGION,
+const sesClient = new SESClient({ region: process.env.AWS_REGION,
     credentials:{
         accessKeyId: process.env.AWS_ACCESS_KEY,
         secretAccessKey: process.env.AWS_SECRET_KEY
